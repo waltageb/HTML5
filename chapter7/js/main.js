0541da061eda45e3b0a08a2e91b4999490b92a2e
waltageb <waltageb@gmail.com>
2018-11-06 12:30:58 -0800

    css & js

--- chapter7/js/main.js
+++ chapter7/js/main.js
@@ -1,1 +1,7 @@
-alert("walta");
+document.getElementById("header1").innerHTML= "no";
+document.getElementsByClassName("header2")[0].innerHTML = "this will work";
+document.getElementsByClassName("header2")[1].innerHTML = "this will work";
+
+document.getElementsByTagName("p")[0].innerHTML = "not lorem";
+document.getElementsByTagName("p")[1].innerHTML = "not lorem";
+document.getElementsByTagName("p")[2].innerHTML = "not lorem";
